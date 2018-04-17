f5f7dfdba28cc1c6d32ed9eefdde8fd8360634e1
BCOM\mbadawi <marwan.badawi@b-com.com>
2018-04-17 14:29:31 +0200

    First implementation of some methods

--- interfaces/SolARPoseHelper.h
+++ interfaces/SolARPoseHelper.h
@@ -30,6 +30,43 @@ class SOLAR_TOOLS_EXPORT_API SolARPoseHelper
 public:
 	SolARPoseHelper();
 	~SolARPoseHelper();
+
+public:
+	/// @brief Transform a point's coordinates using the transform of the Pose
+	static Point3Df transformPoint(const Pose& pose, const Point3Df& point);
+
+	/// @brief Transform a point's coordinates using the transform of the Pose
+	static Vector4f transformPoint(const Pose& pose, const Vector4f& point);
+
+	/// @brief Transform a Pose into the reference frame of another Pose
+	/// @param referencePose : The Pose to use as a reference frame
+	/// @param pose : the Pose to transform into the frame of referencePose
+	static Pose transformPose(const Pose& referencePose, const Pose& pose);
+
+	/// @brief Recover Euler angles of the Pose's rotation
+	/// @return A vector with rotation angles, in radians, with index 0 = rotation around the X-axis, 1 = rotation around Y-axis, 2 = rotation around the Z-axis.
+	static Vector3f getEulerAngles(const Pose& pose);
+
+	/// @brief Create a Pose from Euler angles.
+	/// @param euler A vector with rotation angles, in radians, with index 0 = rotation around the X-axis, 1 = rotation around Y-axis, 2 = rotation around the Z-axis.
+	/// @return A pure rotation Pose with no translation.
+	static Pose createPose(const Vector3f& euler);
+
+	/// @brief Create a Pose from Euler angles.
+	/// @param translation Translation to apply to the Pose
+	/// @param euler A vector with rotation angles, in radians, with index 0 = rotation around the X-axis, 1 = rotation around Y-axis, 2 = rotation around the Z-axis.
+	/// @return A Pose with translation set and rotated with the Euler angles provided.
+	static Pose createPose(const Vector3f& translation, const Vector3f& euler);
+
+	/// \brief constructs a Pose from a rotation matrix of size 3x3 and a translation vector of size 3
+	/// \param rotation_matrix3x3, rotation matrix of size 3x3
+	/// \param translation vector, translation vector of size 3
+	static Pose createPose(const float rotation_matrix3x3[], const float translation_vector[]);
+
+	/// \brief constructs a Pose from a rotation matrix of size 3x3 and a translation vector of size 3
+	/// \param r, rotation matrix of size 3x3
+	/// \param t, translation vector of size 3
+	static Pose createPose(const  RotationMatrixf &r, const  Vector3f & t);
 };
 }
 }
--- src/SolARPoseHelper.cpp
+++ src/SolARPoseHelper.cpp
@@ -29,6 +29,76 @@ SolARPoseHelper::~SolARPoseHelper()
 {
 }
 
+Point3Df SolARPoseHelper::transformPoint(const Pose& pose, const Point3Df& point)
+{
+	Vector4f tmp = transformPoint(pose, Vector4f(point[0], point[1], point[2], 1));
+	return Point3Df(tmp[0], tmp[1], tmp[2]);
+}
+
+Vector4f SolARPoseHelper::transformPoint(const Pose& pose, const Vector4f& point)
+{
+	return pose.getPoseTransform() * point;
+}
+
+Pose SolARPoseHelper::transformPose(const Pose& referencePose, const Pose& pose)
+{
+	Transform3Df referenceInverse = referencePose.getPoseTransform().inverse();
+	Transform3Df transform = pose.getPoseTransform();
+	return Pose(referenceInverse*transform);
+}
+
+Vector3f SolARPoseHelper::getEulerAngles(const Pose& pose)
+{
+	return pose.getPoseTransform().rotation().eulerAngles(0, 1, 2);
+}
+
+Pose SolARPoseHelper::createPose(const Vector3f& euler)
+{
+	Transform3Df transform;
+	transform.setIdentity();
+
+	transform.rotate(Eigen::AngleAxis<float>(euler[0], Vector3f::UnitX()));
+	transform.rotate(Eigen::AngleAxis<float>(euler[1], Vector3f::UnitY()));
+	transform.rotate(Eigen::AngleAxis<float>(euler[2], Vector3f::UnitZ()));
+
+	return Pose(transform);
+}
+
+Pose SolARPoseHelper::createPose(const Vector3f& translation, const Vector3f& euler)
+{
+	Transform3Df transform;
+
+	transform = Translation3Df(translation);
+	transform.rotate(Eigen::AngleAxis<float>(euler[0], Vector3f::UnitX()));
+	transform.rotate(Eigen::AngleAxis<float>(euler[1], Vector3f::UnitY()));
+	transform.rotate(Eigen::AngleAxis<float>(euler[2], Vector3f::UnitZ()));
+
+	return Pose(transform);
+}
+
+Pose SolARPoseHelper::createPose(const float rotation_matrix3x3[], const float translation_vector[])
+{
+	Transform3Df transform;
+	transform.setIdentity();
+
+	//rotation matrix
+	transform(0, 0) = rotation_matrix3x3[0];  transform(0, 1) = rotation_matrix3x3[1];   transform(0, 2) = rotation_matrix3x3[2];
+	transform(1, 0) = rotation_matrix3x3[3];  transform(1, 1) = rotation_matrix3x3[4];   transform(1, 2) = rotation_matrix3x3[5];
+	transform(2, 0) = rotation_matrix3x3[6];  transform(2, 1) = rotation_matrix3x3[7];   transform(2, 2) = rotation_matrix3x3[8];
+
+	//translation vector
+	transform(0, 3) = translation_vector[0];
+	transform(1, 3) = translation_vector[1];
+	transform(2, 3) = translation_vector[2];
+
+	return Pose(transform);
+}
+
+Pose SolARPoseHelper::createPose(const  RotationMatrixf &r, const  Vector3f &t)
+{
+	return createPose(r.data(), t.data());
+}
+
 }
 }
 }
